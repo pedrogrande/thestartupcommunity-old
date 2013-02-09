98ce64cfc8632ef4ddd06c886714e362922ad237
Pete Argent <peter.argent@gmail.com>
2013-02-09 11:45:29 +1100

    aws09-1

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,7 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -48,8 +48,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    "profile-pic.jpg" if original_filename
-  end
+  # def filename
+  #   "profile-pic.jpg" if original_filename
+  # end
 
 end
