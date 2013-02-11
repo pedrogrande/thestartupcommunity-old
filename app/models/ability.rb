0602f766f05c75b62c593da108809862f07ef8cf
Pete Argent <peter.argent@gmail.com>
2013-02-11 11:41:18 +1100

    heroku 20130210-12

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+    else
+      can [:show, :update], User, :id => user.id
+    end
     end
     # Define abilities for the passed in user here. For example:
     #
