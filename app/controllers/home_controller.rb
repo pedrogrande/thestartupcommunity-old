33868b898e33773534bc922d77aefdad1f6ea57e
Pete Argent <peter.argent@gmail.com>
2013-02-11 11:46:25 +1100

    heroku 20130210-13

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :check_for_mobile
   def index
     @users = User.all
-    @business_profiles = BusinessProfile.limit(5)
-    @user_profiles = UserProfile.limit(5)
+    @business_profiles = BusinessProfile.all
+    @user_profiles = UserProfile.all
   end
 end
