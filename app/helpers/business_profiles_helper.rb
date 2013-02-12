ba17d4a6ad0fa0370b8b16ec4e4ee8665c0667a7
Pete Argent <peter.argent@gmail.com>
2013-02-13 10:04:45 +1100

    heroku 20130212-4

--- app/helpers/business_profiles_helper.rb
+++ app/helpers/business_profiles_helper.rb
@@ -1,5 +1,5 @@
 module BusinessProfilesHelper
   def get_name
-    "Check out " + @business_profile.name + "'s profile" + @business_profile.twitter
+    "Check out " + @business_profile.name + "'s profile " + @business_profile.twitter
   end
 end
