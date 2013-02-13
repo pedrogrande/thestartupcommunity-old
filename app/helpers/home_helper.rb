31cfa5e90a4bee96782ddbf6a90edd958de9c935
Pete Argent <peter.argent@gmail.com>
2013-02-13 22:30:05 +1100

    heroku 20130212-10

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def user_profiles_count
+    "Profiles (" + @user_profiles.count.to_s + ")"
+  end
+
+  def business_profiles_count
+    'Startups (' + @business_profiles.count.to_s + ")"
+  end
 end
